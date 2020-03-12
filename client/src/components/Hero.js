18c02ca91869a354b80ee5061fb636e5ea2221d4
moeterani <moe.terani@gmail.com>
2020-03-12 18:03:20 +0100

    Hero Text dynamic

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -3,7 +3,6 @@ import './Hero.css';
 import React from 'react';
 
 export default function Hero({ heroData }) {
-  console.log(heroData);
   const imgUrl = 'https://image.tmdb.org/t/p/w1280' + heroData.backdrop_path;
   const heroStyle = {
     color: 'blue',
@@ -13,11 +12,8 @@ export default function Hero({ heroData }) {
     <div className='hero' style={heroStyle}>
       <div class='heroimage-content'>
         <div class='heroimage-text'>
-          <h1>A Rainy Day in New York</h1>
-          <p>
-            Two young people arrive in New York to spend a weekend, but once
-            they arrive they're met with bad weather and a series of adventures.
-          </p>
+          <h1>{heroData.title}</h1>
+          <p>{heroData.overview}</p>
         </div>
       </div>
     </div>
